989802710f48bf6152c26bd18f3f4459ea5c2cd3
Muhammad Huzaifa <136707896+huzaifa980@users.noreply.github.com>
2023-10-02 01:47:28 +0500

    Add files via upload

---
+++ java script lecture 1/app.js
@@ -0,0 +1,97 @@
+// chapter 1 alertbox
+
+// alert("Enter the task");
+// console.log(alert);
+
+// confirm("welcome to my webpage");
+// console.log(confirm);
+
+// chapter2 variables for strings
+
+// var names = ("huzaifa")
+// console.log(names);
+
+// let fatherNmame = ("imran");
+// console.log(fatherNmame);
+
+// const secondName=("salman");
+// console.log(secondName);
+
+
+// chapter 3 variable for numbers
+
+// let sales = (550);
+// let cost = (460);
+// let profit = (sales-cost);
+// console.log(profit);
+
+// chapter 5 Math expressions: Familiar operators
+
+// let PopularNumber = 3;
+// alert(PopularNumber);
+// alert(PopularNumber + 2);
+
+// let FirtPopularNumber = 5;
+// let SecondPopularNumber = 7;
+// alert(FirtPopularNumber + SecondPopularNumber);
+
+// let PopularNumbers = 12-24;
+// alert(PopularNumbers);
+
+// let PopularNumber = 5*6;
+// console.log(PopularNumber);
+
+// let numbers = 20;
+// let PopularNumber = numbers + 180;
+// console.log(PopularNumber);
+
+// var number = 15;
+// var anotherNumber = 5;
+// var PopularNumber = number + anotherNumber;
+// console.log(PopularNumber);
+
+// var whatsLeftover_in_reminder = 14 % 3;
+// console.log(whatsLeftover_in_reminder);
+
+
+
+// chapter-6 Math expressions: Unfamiliar operators
+
+// let num = 2;
+// let newnum = ++num;
+// console.log(newnum);
+
+// let number = 5;
+// let newNumber = --number;
+// console.log(newNumber);
+
+
+
+// chapter 8 Concatenating text strings
+
+// let x = ("hello");
+// let y = (" everyone");
+// let a = (" welcome to our webpage ");
+// let z = x+y+a;
+// alert (z);
+
+
+
+// chapter 9 Prompts
+
+// var a = prompt("what is your name ?");
+// alert(a);
+// document.write(a);
+
+var a = prompt("Enter your Name");
+var b = prompt("Enter your Email id");
+var c = prompt("enter your password");
+var d = ("completed")
+alert(d);
+
+
+
+
+
+
+
